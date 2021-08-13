2a8a54f457f28c731a18f481739cba38482ebd64
Rodrigo Brito <spensmusic@gmail.com>
2021-08-13 06:32:27 -0300

    refactor: styles

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,9 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: "hello dev" })
+  return response.json({ message: 'hello dev' });
 });
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,7 @@
-import 'reflect-metadata'
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import routes from './routes'
+import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import * as dotenv from 'dotenv';
 
@@ -14,21 +14,22 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError){
-    return response.status(error.statusCode).json({
-      status: 'error',
-      message: error.message
-    });
-  }
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error'
-  })
-})
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log("Server started on port 3333");
-  
-})
+  console.log('Server started on port 3333');
+});
