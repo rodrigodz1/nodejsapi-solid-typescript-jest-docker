ce2c4cd012ce4d408361d405e49cc7948b89234b
Rodrigo Brito <spensmusic@gmail.com>
2021-08-13 09:10:09 -0300

    typeorm adjustments

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,17 +1,6 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'apivendas',
-  migrations: ['./src/shared/typeorm/migrations/*.ts'],
-  cli: {
-    migrationsDir: './src/shared/typeorm/migrations',
-  },
-})
+createConnection()
   .then(connection => {
     // here you can start to work with your entities
   })
