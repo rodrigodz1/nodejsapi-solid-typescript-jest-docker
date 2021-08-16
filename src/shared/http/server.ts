2afa2c5b4c5d7660f38bf0e6186b36bd541b14f9
Rodrigo Brito <spensmusic@gmail.com>
2021-08-16 11:04:54 -0300

    generated static route to images

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,6 +7,7 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import * as dotenv from 'dotenv';
 import '@shared/typeorm';
+import uploadConfig from '@configs/upload';
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
