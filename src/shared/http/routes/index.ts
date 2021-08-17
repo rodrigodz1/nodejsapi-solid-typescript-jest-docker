16ca9ab7faec1b0a14329a4042ebbce479992fd2
Rodrigo Brito <spensmusic@gmail.com>
2021-08-17 07:28:57 -0300

    forgot password route and controller finished

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
